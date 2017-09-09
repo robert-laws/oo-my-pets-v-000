d70dcf5289a31f22eece4853dc93e9caee0f0b4c
Robert Laws <roblaws@me.com>
2017-09-09 08:11:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+ attr_accessor :pets
+ @@all = []
+
+ def initialize
+  #  @pets = { fishes: [], cats: [], dogs: [] }
+   @@all << self
+ end
+
+ def self.all
+   @@all
+ end
+
+ def self.reset_all
+   @@all = []
+ end
+
+ def self.count
+   @@all.size
+ end
+
+ def buy_(name)
+
+ end
 end
